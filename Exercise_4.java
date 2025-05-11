ab161d08c8694e95b97eeacc3212f84748db0d32
Akshay Vijayvergiya <akvv2596@gmail.com>
2025-05-10 23:52:00 -0400

    Exercise_4.java code completed

--- Exercise_4.java
+++ Exercise_4.java
@@ -1,11 +1,47 @@
+import java.util.ArrayList;
 class MergeSort 
 { 
+    // Time Complexity : O(NlogN) -> maximum n iterations in one merge function and logN for number of the tree it created for recursion
+    // Space Complexity : O(n) because of the Arraylist
+    // Did this code successfully run on Leetcode : Yes
+    // Any problem you faced while coding this : No
+   
     // Merges two subarrays of arr[]. 
     // First subarray is arr[l..m] 
     // Second subarray is arr[m+1..r] 
     void merge(int arr[], int l, int m, int r) 
     {  
        //Your code here  
+       int i = l; int j = m+1;
+       ArrayList<Integer> temp = new ArrayList<>();
+       //merging both left and right array by comparing elements
+       while(i<=m && j<=r){
+           if(arr[i] <= arr[j]){
+               temp.add(arr[i]);
+               i++;
+           }
+           else{
+               temp.add(arr[j]);
+               j++;
+           }
+       }
+       //left part remaining elements
+       while(i<=m)
+       {
+            temp.add(arr[i]);
+            i++;
+       }
+       //right part remaining elements
+       while(j<=r)
+       {
+            temp.add(arr[j]);
+            j++;
+       }
+       //putting elements in original array
+       for(i = 0;i<temp.size();i++)
+       {
+           arr[l+i] = temp.get(i);
+       }
     } 
   
     // Main function that sorts arr[l..r] using 
@@ -14,6 +50,16 @@ class MergeSort
     { 
 	//Write your code here
         //Call mergeSort from here 
+        if(l<r)
+        {
+            int mid = l + ((r-l)/2);
+            //left half
+            sort(arr, l, mid);
+            //right half
+            sort(arr, mid+1, r);
+            //finally merging both of the halves while sorting them
+            merge(arr, l, mid, r);
+        }
     } 
   
     /* A utility function to print array of size n */
